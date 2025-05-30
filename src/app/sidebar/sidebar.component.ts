168794b0443934249b448b4a1b45a3f21a5300c5
FatihAydinn <a.fatihaydn@gmail.com>
2025-05-30 23:52:57 +0300

    sidebar click buttons

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,11 +8,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  sidebaritems : { name: string; icon: string } [] = [
-      {name: "ABOUT", icon: "person-fill" },
-      {name: "RESUME", icon: "file-earmark-text" },
-      {name: "WORK", icon: "code-slash" },
-      {name: "CONTACT", icon: "envelope" },
-      {name: "BLOG", icon: "substack" }
+  sidebaritems : { name: string; icon: string, val: number } [] = [
+      {name: "ABOUT", icon: "person-fill", val: 1 },
+      {name: "RESUME", icon: "file-earmark-text", val: 2 },
+      {name: "WORK", icon: "code-slash", val: 3 },
+      {name: "CONTACT", icon: "envelope", val: 4 },
+      {name: "BLOG", icon: "substack", val: 5 }
     ];
+
+    clickedItem: number = 0;
+
+    handleClick(val: number) {
+      this.clickedItem = val;
+      
+      if (this.clickedItem != 1){
+        const element = document.getElementById('sidebarbtn1');
+        if (element)
+          element.style.setProperty('background-color', 'transparent', 'important');
+      }
+    }
 }
